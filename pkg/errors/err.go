04c24b3b6ecba97f73406610b2e5453029207d45
Peter Freese <freese.peter@gmail.com>
2018-12-28 10:14:26 -1000

    test area

--- pkg/errors/err.go
+++ pkg/errors/err.go
@@ -9,7 +9,7 @@ import (
 // a string as value that satisfies error (i.e., implements the Error() method)
 func circleArea(radius float64) (float64, error) {
 	if radius < 0 {
-		return 0, errors.New("Area calculation failed, radius is <0")
+		return 0, errors.New("area calculation failed, radius is <0")
 	}
 	area := math.Pi * radius * radius
 	return area, nil
--- pkg/errors/err_test.go
+++ pkg/errors/err_test.go
@@ -1,21 +1,43 @@
 package errs
 
 import (
+	"gotest.tools/assert"
+	"errors"
 	"math"
 	"testing"
 )
 
 func TestCircleArea(t *testing.T) {
-	var rad float64 = 1
-	actArea, actErr := circleArea(rad)
-
-	var expectedErr error = nil
-	if actErr != nil {
-		t.Errorf("Error was incorrect, got: %v, want: %v.", actErr, expectedErr)
+	// Test examples
+	tables := []struct {
+		r float64
+		expErr	error
+		expArea	float64
+	}{
+		// Radius of 1; no error
+		{
+			1,
+			nil,
+			math.Pi,
+		},
+		// Radius of -1; error
+		{
+			-1,
+			errors.New("area calculation failed, radius is <0"),
+			0,
+		},
 	}
 
-	var expectedArea float64 = math.Pi
-	if actArea != expectedArea {
-		t.Errorf("Area was incorrect, got: %v, want: %v.", actArea, expectedArea)
+	// Go through each test example
+	for _, table := range tables {
+		area, err := circleArea(table.r)
+		// If no error expected, check that the area is within acceptable tolerance
+		if table.expErr == nil {
+			assert.NilError(t, err)
+			assert.Assert(t, math.Abs(area - table.expArea) < 0.0001)
+		} else {
+			// Check that the error message is as expected
+			assert.ErrorContains(t, err, "area calculation failed, radius is <0")
+		}
 	}
 }
